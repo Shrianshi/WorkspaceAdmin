31f6f9cb770d9c07ea6814bf71aa12662b1bd98b
ʀᴀᴊ ᴛʜᴀᴋᴜʀ <rajthakur.kanini@gmail.com>
2023-08-21 10:53:37 +0530

    Updated Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { BookedSuccessComponent } from './components/booked-success/booked-succe
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     FullCalendarModule,
     NoopAnimationsModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
