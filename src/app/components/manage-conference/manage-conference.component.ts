766dac4272410fb87898484631b38d544032ab7b
ʀᴀᴊ ᴛʜᴀᴋᴜʀ <rajthakur.kanini@gmail.com>
2023-08-28 17:47:37 +0530

    Validation added

--- src/app/components/manage-conference/manage-conference.component.ts
+++ src/app/components/manage-conference/manage-conference.component.ts
@@ -3,6 +3,9 @@ import { RoombookingService } from 'src/app/services/bookingservice/roombooking.
 import { LocationService } from 'src/app/services/location.service';
 import { RoomService } from 'src/app/services/roomservice/room.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { WorkspaceFilterService } from 'src/app/services/workspaceFilters/workspace-filter.service';
+
 
 interface Card {
   ImageUrl: string;
@@ -18,19 +21,33 @@ interface Card {
 })
 export class ManageConferenceComponent implements OnInit {
   header: string = 'Conference Rooms'
-  constructor(private roomSer: RoomService, private locSer: LocationService, private toast: ToastrService) { }
+  roomForm: FormGroup;
+
+  constructor(private roomSer: RoomService, private locSer: LocationService, private toast: ToastrService,
+    private fb: FormBuilder, private wsService: WorkspaceFilterService) { 
+      this.roomForm = this.fb.group({
+        roomName: new FormControl('',[Validators.required]),    
+        roomCapacity: new FormControl('',[Validators.required]),
+        locationId: new FormControl(1, [Validators.required]),
+        amenities: new FormControl([],[Validators.required])
+      });
+    }
+
   cards: any[] = []
   locations: any[] = []
+  locationName: string ='All'
 
   newRoom: any = {
     imageData: '',
     roomName: '',
-    locationId: 1,
-    roomCapacity: 5,
+    locationId: null,
+    roomCapacity: null,
     amenities: []
   };
+
   amenities: string[] = ['TV', 'Whiteboard', 'Wi-Fi', 'Digital Projector'];
   selectedAmenities: { [key: string]: boolean } = {};
+
   onFileSelected(event: any) {
     const file = event.target.files[0];
     const fileReader = new FileReader();
@@ -44,6 +61,7 @@ export class ManageConferenceComponent implements OnInit {
     }
     fileReader.readAsArrayBuffer(file);
   }
+
   ngOnInit(): void {
     this.locSer.getAllLocation().subscribe((data) => {
       this.locations = data
@@ -57,12 +75,14 @@ export class ManageConferenceComponent implements OnInit {
       console.log(error)
     })
   }
+
   addRoom() {
     const selectedAmenitiesList = Object.keys(this.selectedAmenities).filter(amenity => this.selectedAmenities[amenity]);
     console.log('Selected Amenities:', selectedAmenitiesList);
     const amenitiesJson = JSON.stringify(selectedAmenitiesList);
     this.newRoom.amenities = amenitiesJson;
-    console.log("Final data", this.newRoom);
+    // console.log("Final data", this.newRoom);
+    
     this.roomSer.addRoom(this.newRoom).subscribe((data) => {
       this.toast.success('Room Added')
       console.log(data);
@@ -70,7 +90,26 @@ export class ManageConferenceComponent implements OnInit {
   }
 
 
-
-
-
+  changeLocationHandler() {
+    this.roomOnLocation(this.locationName)
+  }
+  roomOnLocation(locationName: string) {
+    if (locationName == "All") {
+      this.roomSer.getAllRoom().subscribe((data) => {
+        this.cards = data
+      }, (error) => {
+        console.log(error)
+      })
+    }
+    else {
+      this.wsService.getRoomsByLocation(locationName).subscribe((data) => {
+        this.cards = []
+        this.cards = data;
+      }, (error) => {
+        console.log(error)
+      })
+    }
+  }
 }
+
+
